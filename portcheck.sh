d7fe8d4dd3aabf4a6e0a49fff08374ed635bfa35
Robert Tulke <rt@debian.sh>
2025-05-05 11:23:06 +0200

    Update portcheck.sh

--- portcheck.sh
+++ portcheck.sh
@@ -42,14 +42,14 @@ tail -n +2 /proc/net/tcp | while read -r line; do
     for pid in $(ls -d /proc/[0-9]* 2>/dev/null); do
         cmdline=$(cat "$pid/comm" 2>/dev/null)
         uid=$(awk '/Uid:/ {print $2}' "$pid/status" 2>/dev/null)
-        user=$(getent passwd "$uid" | cut -d: -f1)
+        user=$(getent passwd "$uid" | awk -F: '{print $1}')
 
         # Search all FDs after the inode
         for fd in "$pid"/fd/*; do
             [ -L "$fd" ] || continue
             link=$(readlink "$fd" 2>/dev/null)
             if [[ "$link" == socket:* ]]; then
-                fd_inode=$(echo "$link" | grep -oP '\[\K[0-9]+')
+                fd_inode=$(awk -F'\\[|\\]' '{print $2}' <<< "$link")
                 if [[ "$fd_inode" == "$inode" ]]; then
                     printf "%-9s %-6s %-6s %-21s %-21s %s\n" "$cmdline" "${pid##*/}" "$user" "$local_addr_parsed" "$rem_addr_parsed" "$state"
                     break
